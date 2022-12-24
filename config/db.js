8a2ac0920850be4e1aaa2e79203f777999fff109
Rahul <rahulkashyap4019@gmail.com>
2022-12-24 10:16:47 +0530

    Connect server to the DB

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectWithDb = () => {
+  mongoose.set('strictQuery', false);
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log(`DB connected successfully`))
+    .catch((error) => {
+      console.log(`DB connection failed`);
+      console.log(error);
+      process.exit(1);
+    });
+};
+
+module.exports = connectWithDb;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectWithDb = require('./config/db');
+require('dotenv').config();
 const app = express();
 
-const port = 8000;
+// connect to the database
+connectWithDb();
 
 // regular middlewares
 app.use(express.json());
@@ -10,9 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 // use express router
 app.use('/', require('./routes'));
 
-app.listen(port, (error) => {
+app.listen(process.env.PORT || 5000, (error) => {
   if (error) {
     console.log(`Error in running server: ${error}`);
   }
-  console.log(`Server is up and running at port: ${port}`);
+  console.log(`Server is up and running at port: ${process.env.PORT}`);
 });
